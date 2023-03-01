fa3091f68584941a2ea55dcd20d7bea6a3358072
Sheepzy8 <lonesheep8@163.com>
2023-03-01 22:14:00 +0800

    modify hello.cpp, add new addtwonums function

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
-
+int addtwonums(int a, int b)
+{
+    return a + b;
+}
 int main(){
     cout << "hello world!" << endl;
+    
+    int a = 1;
+    int b = 2;
+
+    cout << addtwonums(a, b);
 }
