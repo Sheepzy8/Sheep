42df1790ed41dc0282b8640e49b077d8aeb1b0bb
Sheepzy8 <lonesheep8@163.com>
2023-03-01 22:43:48 +0800

    add new printstring function on testbranch1's hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
+#include<string>
+
 using namespace std;
 int addtwonums(int a, int b)
 {
     return a + b;
+}
+void printfsting(string &s)
+{
+    cout << s << endl;
 }
 int main(){
     cout << "hello world!" << endl;
     
     int a = 1;
     int b = 2;
+    string s = "yangzhiyong is a smart boy, hahaha!";
 
+    printfsting(s);
     cout << addtwonums(a, b);
     cout << "yangzhiyong i love you!" << endl;
 }
